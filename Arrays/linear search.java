b9a78346f1d60ac5f009041b94d132ab06cc4085
Vivek Shukla <vivekshukla0552@gmail.com>
2026-06-11 09:36:12 +0530

    Create linear search.java

---
+++ Arrays/linear search.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int search(int arr[], int x) {
+        // code here
+        for(int idx=0;idx<arr.length;idx++){
+            if(arr[idx]==x){
+                return idx;
+            }
+        }
+        return -1;
+        
+    }
+}
