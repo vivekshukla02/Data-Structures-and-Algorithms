3d3089035aa7ad1d290e58b288f9cab9d808cf8f
Vivek Shukla <vivekshukla0552@gmail.com>
2026-06-12 20:53:17 +0530

    Update LinearSearch.java

--- Arrays/LinearSearch.java
+++ Arrays/LinearSearch.java
@@ -2,7 +2,7 @@ class Solution {
     public int search(int arr[], int x) {
         // code here
         for(int idx=0;idx<arr.length;idx++){
-            if(arr[idx]==x){
+            if(arr[idx]=y){
                 return idx;
             }
         }
